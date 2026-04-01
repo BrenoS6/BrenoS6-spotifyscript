6dfb7c9990b32b1eda375e46e6844bf8de9cf451
Breno Miranda <156455245+BrenoS6@users.noreply.github.com>
2026-03-31 21:12:00 -0300

    Add initial configuration settings in Config.lua

---
+++ Config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.OpenCommand = 'music'
+Config.AdminCommand = 'musicadmin'
+Config.DefaultVolume = 0.5
+
+Config.AdminIdentifiers = {
+    -- Coloque seu license aqui, ex:
+    -- 'license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+}
